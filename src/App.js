2d38e00d689a699e5ab9dafdb4ec29f2a72909e6
maryredlinger <mary@redlinger.net>
2020-06-25 21:53:58 -0700

    adds home features and nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Nav, NavItem, NavLink as Link } from 'reactstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home.js'
+ 
 
 function App() {
   return (
     <div className="App">
       <h1>Joe Bernans Portoflio Site</h1>
+        <h2>Teacher, Tutor, Theologian</h2>
+        <div >
+          <Nav className="navbar navbar-dark bg-primary">
+            <Link className="navbar-brand" href="#">Home</Link>
+            
+          <div id="navbarColor01">
+            <ul className="NavFlex navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link className="nav-link" href="#">Home <span className="sr-only">(current)</span></Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" href="#">Features</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" href="#">Pricing</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" href="#">About</Link>
+              </li>
+            </ul>
+          </div>
+          </Nav>
+        </div>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Home} />
+          </Switch>
+        </Router>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div className="ImgBack">
+            <h1>image</h1>
+           
+        </div>
+    )
+}
+
+export default Home;
