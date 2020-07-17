cb25fedf9cabb03e9c2ed9b8706093437ec93164
maryredlinger <mary@redlinger.net>
2020-07-17 16:39:32 -0700

    english course

--- src/pages/English.js
+++ src/pages/English.js
@@ -1,17 +1,67 @@
 import React from 'react';
+import '../styles/English.css'
 
 const English = () => {
     return(
-        <div>
+        <div className="english-container">
             <h1>English</h1>
             <div>
                 <h1>Reading Skills / Remediation</h1>
+                <h3>Phonics / Decoding</h3>
+                <h3>Comprehension</h3>
+                <h3>Reading Rate</h3>
+                <h3>Reading Styles</h3>
+                    <ul>
+                        <li>Phrase, SKimming, Scanning</li>
+                        <li>Critical: SQ4R</li>
+                        <li>Genre: Textbook vs. Other Types of Literature</li>
+                    </ul>
             </div>
             <div>
                 <h1>Grammar</h1>
+                <h3>Parts of Speech</h3>
+                <h3>Phrases, Clauses</h3>
+                <h3>Usage: World Usage and Agreement</h3>
+                <h3>Mechanics: Capitalization, Abbreviation, Punctuation</h3>
+                <h3>Sentences</h3>
+                    <ul>
+                        <li>Types of Sentences: Simple Compound, Complex, Compound-Complex</li>
+                        <li>Functions of Sentences: Declarative, Interrogative, Imperative, Exclamatory</li>
+                        <li>Sentence Patterns</li>
+                        <li>Sentence Combing</li>
+                        <li>Diagramming</li>
+                    </ul>
             </div>
             <div>
                 <h1>Literature</h1>
+                <h3>Genres: Types of</h3>
+                    <ul>
+                        <li>Fiction / Nonfiction</li>
+                        <li>Poetry / Prose</li>
+                        <li>Drama</li>
+                    </ul>
+                <h3>Word Study:</h3>
+                    <ul>
+                        <li>Vocabulary / Spelling</li>
+                        <li>Definitions</li>
+                        <li>Synonyms / Antonyms</li>
+                        <li>Derivations</li>
+                        <li>Etymology / Roots</li>
+                    </ul>
+                <h3>Literary Devices: to include, but not limites to</h3>
+                    <ul>
+                        <li>Imagery</li>
+                        <li>Plot / Plot Structure</li>
+                        <li>Authors Point of View</li>
+                        <li>Character</li>
+                        <li>Setting</li>
+                        <li>Tone</li>
+                        <li>Theme</li>
+                        <li>Irony</li>
+                        <li>Foreshadowing / Flashback</li>
+                        <li>Rhyme / Meter</li>
+                        <li>Alliteration / Assonance</li>
+                    </ul>
             </div>
             <div>
                 <h1>Writing <i>Composition / Writer's Techniques</i></h1>
--- src/pages/Math.js
+++ src/pages/Math.js
@@ -4,6 +4,24 @@ const Math = () => {
     return(
         <div>
             <h1>Math</h1>
+            <div>
+                <h1>World Religions</h1>
+            </div>
+            <div>
+                <h1>Introduction</h1>
+            </div>
+            <div>
+                <h1>The Primal Religions <i>Indigenous Religions</i></h1>
+            </div>
+            <div>
+                <h1>Earliest Religions <i>Traditions from the East - Hinduism, Buddhism</i></h1>
+            </div>
+            <div>
+                <h1>Chinese Wisdom Traditions</h1>
+            </div>
+            <div>
+                <h1>Religions from Revelation <i>Judaism, CHristianity, Islam</i></h1>
+            </div>
         </div>
     )
 }
