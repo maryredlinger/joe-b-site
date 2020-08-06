b0c824fc5ab0213355037cd7adaceb140ce6ff60
maryredlinger <mary@redlinger.net>
2020-08-05 17:14:54 -0700

    courses page

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Nav, NavItem, NavLink as Link } from 'reactstrap';
 
 const Courses = () => {
     return(
-        <div>
-            <h1>MAth</h1>
-            <img src="./english_reading.jpg"></img>
-            <img src="./math_social.png"></img>
+        <div className="coursesContainer">
+            <img className="coursesImage" src="/courses.jpg"></img>
+            <div className="centered">
+            <h1>This 2020 - 2021 School Year, I will be offering 2 Literature courses</h1>
+            <h2>on Tuesday's and Thursday's</h2>
+            <Link href="/contact"><button className="btn btn-primary">Contact Me to Sign Up</button></Link>
+            </div>
         </div>
     )
 }
